7ae321c65c00e65a1825024fbf020d0fe50eeb89
arpan <shaharpan05@gmail.com>
2020-08-03 10:20:38 +0530

    add  build

---
+++ public/precache-manifest.6e69a9e9fbd4407a4b2cf56fd8038fc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f04f19d57ee469c24b9502b8a46ce99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbe3e6e6a83809002583",
+    "url": "/static/css/2.60d9b82a.chunk.css"
+  },
+  {
+    "revision": "a408d210636eb5240028",
+    "url": "/static/css/main.c4524e97.chunk.css"
+  },
+  {
+    "revision": "bbe3e6e6a83809002583",
+    "url": "/static/js/2.8a37794a.chunk.js"
+  },
+  {
+    "revision": "48079c56f55591f90a7c540a20d04151",
+    "url": "/static/js/2.8a37794a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a408d210636eb5240028",
+    "url": "/static/js/main.f0dda910.chunk.js"
+  },
+  {
+    "revision": "7d6a1dd8807bd500b4ee",
+    "url": "/static/js/runtime-main.5ae2e513.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
