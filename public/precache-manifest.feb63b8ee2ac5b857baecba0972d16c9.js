d8ec2498cdf4c586520fd25575763664295149c5
arpan <shaharpan05@gmail.com>
2020-08-12 20:44:42 +0530

    add frontend  build

---
+++ public/precache-manifest.feb63b8ee2ac5b857baecba0972d16c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c98752ac7f8ac8a14a6c43276acaa03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a955e4196109325ba11a",
+    "url": "/static/css/2.60d9b82a.chunk.css"
+  },
+  {
+    "revision": "e6134c50e61d46bb71f9",
+    "url": "/static/css/main.0629f142.chunk.css"
+  },
+  {
+    "revision": "a955e4196109325ba11a",
+    "url": "/static/js/2.5debbf79.chunk.js"
+  },
+  {
+    "revision": "48079c56f55591f90a7c540a20d04151",
+    "url": "/static/js/2.5debbf79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6134c50e61d46bb71f9",
+    "url": "/static/js/main.bf8c5ff4.chunk.js"
+  },
+  {
+    "revision": "7d6a1dd8807bd500b4ee",
+    "url": "/static/js/runtime-main.5ae2e513.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.307e6048d7b0f3c6605798ae1a84ccad.js"
+  "/precache-manifest.feb63b8ee2ac5b857baecba0972d16c9.js"
 );
 
 self.addEventListener('message', (event) => {
